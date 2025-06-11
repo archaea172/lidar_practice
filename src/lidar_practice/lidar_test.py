41ee5b019854635e4c25ff04f57e108a464c9590
aky <akiyugata271@gmail.com>
2025-06-11 22:32:38 +0900

    一旦終わり

--- src/lidar_practice/lidar_test.py
+++ src/lidar_practice/lidar_test.py
@@ -2,6 +2,9 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 
+import numpy as np
+import cv2
+
 class LidarTestNode(Node):
     def __init__(self):
         super().__init__('lidar_test_node')
@@ -13,11 +16,35 @@ class LidarTestNode(Node):
             self.lidar_callback,
             10
         )
+
+        self.ransac_timer_callback = self.create_timer(
+            0.1,
+            self.timer_callback
+        )
+        self.point = np.ndarray((455, 2))
         
 
     def lidar_callback(self, rxdata:LaserScan):
         self.range = rxdata.ranges
-        self.get_logger().info(str(self.range[226]))
+        
+        for i in range(len(self.range)):
+            try:
+                angle = 2*np.pi/len(self.range)*i
+                zahyo = np.array([np.cos(angle), np.sin(angle)])*self.range[i]*100
+                self.point[i] = zahyo.copy
+
+            except:
+                continue
+        
+    def timer_callback(self):
+        img = np.full((700, 700, 3), 128, dtype=np.uint8)
+        
+        for i in range(455):
+            # cv2.circle(img, self.point[i], 1, (255, 0, 0), -1)
+        # cv2.imshow("lidar", img)
+            pass
+        self.get_logger().info(str(self.point[0]))
+        cv2.waitKey(3)
         
 
 def main_lidar_test():
