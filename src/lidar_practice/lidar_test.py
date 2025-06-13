0bd17d17451dfdf89b4ff477435b490bc11d04cb
aky <akiyugata271@gmail.com>
2025-06-13 19:01:52 +0900

    動いた！

--- src/lidar_practice/lidar_test.py
+++ src/lidar_practice/lidar_test.py
@@ -3,35 +3,34 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 
 import numpy as np
-import cv2
 
 class LidarTestNode(Node):
     def __init__(self):
         super().__init__('lidar_test_node')
-        self.range = []
-
         self.lidar_sub = self.create_subscription(
             LaserScan,
-            'scan',
+            '/ldlidar_node/scan',
             self.lidar_callback,
             10
         )
-
+        """
         self.ransac_timer_callback = self.create_timer(
-            0.1,
+            1,
             self.timer_callback
-        )
-        self.point = np.ndarray((455, 2))
+        )"""
         
 
     def lidar_callback(self, rxdata:LaserScan):
         angle = rxdata.angle_min
-        self.point_zahyo = np.ndarray((len(rxdata.ranges), 2))
-        for point in rxdata.ranges:
-            zahyo = np.array([np.cos(angle), np.sin(angle)])*point
+        self.point = np.array([
+            [0, 0]
+        ])
+        for range in rxdata.ranges:
+            zahyo = np.array([np.cos(angle), np.sin(angle)])*range
             angle += rxdata.angle_increment
+            self.point = np.vstack([self.point, zahyo])
+        self.get_logger().info(str(self.point.shape))
 
-        self.get_logger().info(str(zahyo[224]))
         
     def timer_callback(self):
         pass
@@ -41,7 +40,8 @@ def main_lidar_test():
     rclpy.init()
     lidar_test_node = LidarTestNode()
     try:
-        rclpy.spin(lidar_test_node)
+        rclpy.spin_once(lidar_test_node)
+
     except KeyboardInterrupt:
         pass
     finally:
